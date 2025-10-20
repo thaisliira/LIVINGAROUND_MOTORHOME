6bfe5107c1598a733212635ff8f5a3f44ae52ea7
Thais <thais__lira@hotmail.com>
2025-10-20 21:47:25 +0100

    oi

---
+++ script/script.js
@@ -0,0 +1,80 @@
+// BANCO DE DADOS ESTATICO JS
+const destinosSuportados = {
+    'Brasil': ['Rio de Janeiro','Recife'],
+    'Portugal': ['Porto', 'Lisboa'],
+};
+
+// FUNCAO PARA PREENCHER LISTA DE DESTINOS
+function atualizarCidades() {
+    const selectPais = document.getElementById('select-pais');
+    const selectCidade = document.getElementById('select-cidade');
+    
+    const paisSelecionado = selectPais.value;
+
+    // LIMPA A LISTA E ADICIONA OPCAO PADRAO
+    selectCidade.innerHTML = '<option selected value="">Cidade</option>'; 
+    
+    // Verifica se um país válido foi selecionado
+    if (paisSelecionado && destinosSuportados[paisSelecionado]) {
+        const cidades = destinosSuportados[paisSelecionado];
+
+        // PREENCHE LISTA DE CIDADES
+        cidades.forEach(cidade => {
+            const option = document.createElement('option');
+            option.value = cidade;
+            option.textContent = cidade;
+            selectCidade.appendChild(option);
+        });
+    }
+}
+
+// PARA CARREGAR MAPA NO IFRAME
+function abrirMapa() {
+    const selectPais = document.getElementById('select-pais');
+    const selectCidade = document.getElementById('select-cidade');
+    const mapaFrame = document.getElementById('mapa-frame');
+    
+    const pais = selectPais.value;
+    const cidade = selectCidade.value;
+
+    // Validação
+    if (!pais || !cidade) {
+        alert("Por favor, selecione um País e uma Cidade válidos.");
+        // Limpa o mapa se a seleção for inválida
+        mapaFrame.src = "about:blank"; 
+        return; 
+    }
+    
+    // PESQUISA DESTINO E EXIBE NO MAPA UTILIZANDO A PESQUISA DO GOOGLE
+    const termoPesquisa = encodeURIComponent(`${cidade}, ${pais}`);
+
+    const urlFinal = `https://maps.google.com/maps?q=${termoPesquisa}&output=embed`;
+
+    mapaFrame.src = urlFinal;
+
+    // ENCONTRA O ELEMENTO
+    var mapaDiv = document.getElementById('map');
+
+    // TORNA A DIV DO MAPA VISÍVEL
+    mapaDiv.style.display = 'block'; // Ou 'flex', dependendo do seu layout
+}
+
+
+
+// Chame esta função uma vez no início para garantir que o mapa esteja vazio ou em um local padrão.
+document.addEventListener('DOMContentLoaded', () => {
+    // Isso garante que a lista de cidades está vazia e o mapa está 'limpo' ao carregar.
+    atualizarCidades(); 
+});
+
+function validar(){
+
+        
+    var email=document.formulario.email.value;
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!re.test(String(email).toLowerCase())) {
+            alert("Email Invalido.");
+            return false;
+        }
+        alert("Obrigado pelo preechimento do formulário.")
+        return true;}
